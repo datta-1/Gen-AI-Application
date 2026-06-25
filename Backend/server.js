cb0034fb6d6f806a75a6022978a9b52ac4adc840
Narasimha <chundurunarasimhadatta@outlook.com>
2026-06-25 15:45:12 +0530

    Added the Gemini API Key

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 
 const app=require("./src/app")
 const connectDB=require("./src/config/database")
+const invokeGeminiai=require("./src/services/ai.service")
 
 connectDB()
 
